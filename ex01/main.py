f0bba90dce1227df062065bd8e14d223886fa8c3
lucasdeoliveira1 <lucasalfindo@hotmail.com>
2018-09-20 20:10:44 -0300

    Versão inicial do meu projeto

--- ex01/main.py
+++ ex01/main.py
@@ -1,2 +1,18 @@
-print("Oi, mundo")
-print("Salve quebra")
+
+import math
+
+a = int(input("Informe o valor de a: "))
+b = int(input("Informe o valor de b: "))
+c = int(input("Informe o valor de c: "))
+
+delta = b * b - 4 * a * c
+
+print("Delta: ", delta)
+
+if delta >= 0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print("Raiz x1: ", x1)
+    print("Raiz x2: ", x2)
+else:
+    print("Nao existem raízes reais")
